2f88c507f84d093d2a05671647cdbad0d15c0773
Edward Shannon <edward.01s2@gmail.com>
2021-09-27 16:01:07 -0600

    Initial

---
+++ src/Fizz.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+final class FizzBuzz {
+
+     
+    public function convert($num) {
+
+        //Check if divisible by 15
+        if ($num % 15 === 0) {
+            return "fizzbuzz";
+        }
+        //Check if divisible by 3
+        if($num % 3 == 0){
+            return "fizz";
+        }
+        //Check if divisible by 5
+        if($num % 5 == 0){
+            return "buzz";
+        }
+
+        return strval($num);
+        
+    }
+
+    public function run() {
+
+        $out = [];
+
+        //Set max range
+        for($i = 1; $i < 21; $i++) {
+            $out[$i] = $this->convert($i);
+        }
+
+        echo implode(" ", $out) . PHP_EOL;
+    }
+}
+
+$fizz = new FizzBuzz;
+$fizz->run();
+
+?>
